af31f22f6cd4557be27e49fad278c5f9bc495403
Celina Lopez <marisa.celina.lopez4@gmail.com>
2025-04-16 19:31:12 -0700

    update gem changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Change Log
+
+## [Unreleased] - 2025-??-??
+
+Adding sections on how to add new tags, documentation. Right now this is a very simple look.
+
+## [0.0.1] - 2025-04-16
+
+### Added
+
+- this todo list!
+
+### Changed
+
+### Fixed
--- app/controllers/prettytodo/notes_controller.rb
+++ app/controllers/prettytodo/notes_controller.rb
@@ -1,9 +1,19 @@
 class Prettytodo::NotesController < Prettytodo::ApplicationController
+  PrettyTodoStruct = Struct.new(:tag, :text, :line, :file_name)
   def index
-    @annotations = Rails::SourceAnnotationExtractor.new(
+    annotations = Rails::SourceAnnotationExtractor.new(
       Rails::SourceAnnotationExtractor::Annotation.tags.join("|")
     ).find(
       Rails::SourceAnnotationExtractor::Annotation.directories
     )
+    pretty_annotations = []
+    annotations.keys.each do |file_name|
+      annotations[file_name].each do |todo|
+      pretty_annotations << PrettyTodoStruct.new(
+        todo.tag, todo.text, todo.line, file_name
+      )
+      end
+    end
+    @annotations_by_tag = pretty_annotations.group_by(&:tag)
   end
 end
